e92f8631ca0216515985d38b9491a7b918736106
Léo Pradel <pradel.leo@gmail.com>
2023-01-10 18:23:29 +0100

    create a did session

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { Inter } from "@next/font/google";
 import { showConnect, UserData } from "@stacks/connect";
 import { useEffect, useState } from "react";
+import { DIDSession } from "did-session";
+import { StacksWebAuth, getAccountId } from "@didtools/pkh-stacks";
 import styles from "../styles/Home.module.css";
 import { userSession } from "../userSession";
 
@@ -8,6 +10,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [user, setUser] = useState<UserData>();
+
+  useEffect(() => {
+    const userData = userSession.loadUserData();
+    if (userData) {
+      setUser(userData);
+    }
+  }, []);
 
   const handleConnect = () =>
     showConnect({
@@ -22,14 +31,33 @@ export default function Home() {
       },
     });
 
-  useEffect(() => {
-    const userData = userSession.loadUserData();
-    if (userData) {
-      setUser(userData);
-    }
-  }, []);
+  const handleDisconnect = () => {
+    userSession.signUserOut();
+    setUser(undefined);
+  };
 
-  console.log(user);
+  const handleTest = async () => {
+    if (!user) return;
+
+    const stacksProvider = window.StacksProvider;
+    const address = user.profile.stxAddress.mainnet;
+
+    const accountId = await getAccountId(stacksProvider, address);
+    console.log({ accountId });
+
+    const authMethod = await StacksWebAuth.getAuthMethod(
+      stacksProvider,
+      accountId
+    );
+    console.log({ authMethod });
+
+    const session = await DIDSession.authorize(authMethod, {
+      resources: ["ceramic://*"],
+    });
+    console.log({ session });
+    // @ts-ignore
+    console.log({ signature: session.cacao.s.s });
+  };
 
   return (
     <main className={`${inter.className} ${styles.main}`}>
@@ -37,8 +65,9 @@ export default function Home() {
       {!user && <button onClick={handleConnect}>Connect Stacks wallet</button>}
 
       {/* User logged in we can interact with Ceramic */}
+      {user && <button onClick={handleDisconnect}>Disconnect</button>}
       {user && <p>Connected: {user.profile.stxAddress.mainnet}</p>}
-      {user && <button>Connect to Ceramic</button>}
+      {user && <button onClick={handleTest}>Run test</button>}
     </main>
   );
 }
